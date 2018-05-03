92c345405b9e89c24802fcb141c04deccb84d835
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-05-03 23:00:51 +0900

    バリデーションとテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.email.downcase!}
   validates :family_name,
             presence: true,
             length: {maximum: 25}
@@ -8,8 +9,10 @@ class User < ApplicationRecord
   validates :email,
             presence: true,
             length: {maximum: 255},
-            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
+            format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
             uniqueness: {case_sensitive: false}
-
+  validates :password,
+            presence: true,
+            length: {minimum: 6}
   has_secure_password
 end
