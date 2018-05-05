29094774010b20d3ca280b2f2ee7b64a12744a9b
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-05-05 14:32:53 +0900

    完成！

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   def set_available_tags_to_gon
     gon.available_tags = Tag.all.pluck(:name)
   end
+  def circle_list
+    code = ""
+    Circle.all.each do |circle|
+      code += circle_view(circle)
+    end
+    code
+  end
+  private
+  def circle_view(circle)
+    code = "<div class='col-6'>"
+    code += "<div style='position:relative;width:100%;height:100%'>"
+    code += "<a href='/circles/#{circle.id}'>"
+    code += "<img src='https://mb.api.cloud.nifty.com/2013-09-01/applications/6aJWFXEYlhr6utde/publicFiles/#{circle.id}.jpg' style='width:100%;height:auto;'>"
+    code += "</a>"
+    code += "<div style='position:absolute;left:10px;bottom:10px;color:#ffffff;font-size:2em;text-shadow: 0 0 3px black;'>#{circle.name}</div></a>"
+    code += "</div>"
+    code += "</div>"
+    code
+  end
+
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,14 +2,25 @@ module StaticPagesHelper
   def circle_suggest
     code = ""
     current_user.tags.each do |tag|
-      code += tag.name
+      code += "<div class='row'><div class='col-12'><div style='background:#1f1f1f;color:#ffffff;padding:5px 15px;'>#{tag.name}</div></div></div>";
+      code += "<div class='row'>"
       tag.circles.each do |circle|
-        code += circle.name
+        code += circle_view(circle)
       end
+      code += "</div>"
     end
     code
   end
   private
-  def circle_view
+  def circle_view(circle)
+    code = "<div class='col-6'>"
+    code += "<div style='position:relative;width:100%;height:100%'>"
+    code += "<a href='/circles/#{circle.id}'>"
+    code += "<img src='https://mb.api.cloud.nifty.com/2013-09-01/applications/6aJWFXEYlhr6utde/publicFiles/#{circle.id}.jpg' style='width:100%;height:auto;'>"
+    code += "</a>"
+    code += "<div style='position:absolute;left:10px;bottom:10px;color:#ffffff;font-size:2em;text-shadow: 0 0 3px black;'>#{circle.name}</div></a>"
+    code += "</div>"
+    code += "</div>"
+    code
   end
 end
