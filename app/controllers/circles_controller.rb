87717d45fc7627fadb0746d5e4a77a59993d8b17
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-05-05 23:52:43 +0900

    フォントと若干のデータ変更

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,5 +1,6 @@
 class CirclesController < ApplicationController
   before_action :set_circle, only: [:show, :edit, :update, :destroy, :new_member, :create_member]
+  before_action :require_login
 
   # GET /circles
   # GET /circles.json
